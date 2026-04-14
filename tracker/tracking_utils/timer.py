d0d8f2b68ab58a774eb37d4e6202981854944051
aircampro <147149143+aircampro@users.noreply.github.com>
2026-04-14 15:38:38 +0100

    Update timer.py
    state engine round to ensure correct timer order

--- tracker/tracking_utils/timer.py
+++ tracker/tracking_utils/timer.py
@@ -1,6 +1,5 @@
 import time
 
-
 class Timer(object):
     """A simple timer."""
     def __init__(self):
@@ -9,29 +8,32 @@ class Timer(object):
         self.start_time = 0.
         self.diff = 0.
         self.average_time = 0.
-
         self.duration = 0.
-
+        self.state = 0
     def tic(self):
         # using time.time instead of time.clock because time time.clock
         # does not normalize for multithreading
         self.start_time = time.time()
-
+        self.state = 1
     def toc(self, average=True):
-        self.diff = time.time() - self.start_time
-        self.total_time += self.diff
-        self.calls += 1
-        self.average_time = self.total_time / self.calls
-        if average:
-            self.duration = self.average_time
+        if self.state == 1:
+            self.diff = time.time() - self.start_time
+            self.total_time += self.diff
+            self.calls += 1
+            self.average_time = self.total_time / self.calls
+            if average:
+                self.duration = self.average_time
+            else:
+                self.duration = self.diff
         else:
-            self.duration = self.diff
+            print("issue tic first to begin timer")
+            self.duration = 0.
         return self.duration
-
     def clear(self):
         self.total_time = 0.
         self.calls = 0
         self.start_time = 0.
         self.diff = 0.
         self.average_time = 0.
-        self.duration = 0.
+        self.duration = 0
+        self.state = 0
